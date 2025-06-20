8c4f6f299704db5841eb5bbab3d2950491b13e04
nganga96 <hongapau96@gmail.com>
2025-06-20 10:54:49 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,12 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.spatial.io/s/Room-Spring_Natural-Eden-Hotel-679c550fb405497f2f1b93c1?share=8927061944971499510',
+	'https://www.spatial.io/s/Room-Spring_Natural-Eden-Hotel-679ca320fa1f8fa9581670c7?share=6574539164448796491',
+	'https://www.spatial.io/s/Room-summer_Natural-Eden-Hotel-6815f8707b2839f34730295d?share=4024515671318689825',
+	'https://www.spatial.io/s/Room-autumn_Natural-Eden-Hotel-6815fce37b2839f3473032e4?share=685171731985255076',
+	'https://www.spatial.io/s/Room-winter_Natural-Eden-Hotel-681600617b2839f3473037e7?share=6849748684717796957',
+	'https://www.spatial.io/s/Room-winter_Natural-Eden-Hotel-6819afd57bd4da9adb59b396?share=4093947779920874438',
+	'https://www.spatial.io/s/Room-summer_Natural-Eden-Hotel-6820786e64ab4b97de87cb55?share=7370245815155646796',
+	'https://www.spatial.io/s/Room-autumn_Natural-Eden-Hotel-682078a684cc77b7f11bdd48?share=3904002368122428796',
 ];
